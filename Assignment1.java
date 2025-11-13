4fea24f91b286d52ce76d7014e1a4f1e1d977773
LevianVian <artalevi4@gmail.com>
2025-11-13 19:36:16 +0700

    Create Assignment1.java to implement a right-aligned numeric triangle printing program based on user input

---
+++ Assignment1.java
@@ -0,0 +1,26 @@
+// NumericTriangle.java
+import java.util.Scanner;
+
+public class Assignment1 {
+    public static void printRightAlignedNumericTriangle(int N) {
+        for (int i = 1; i <= N; i++) {
+
+            for (int s = 0; s < N - i; s++) System.out.print(" ");
+
+            for (int k = 1; k <= i; k++) System.out.print(k);
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of N (0 - 5): ");
+        int N = sc.nextInt();
+
+        printRightAlignedNumericTriangle(N);
+        System.out.println();
+        printRightAlignedNumericTriangle(N);
+
+        sc.close();
+    }
+}
